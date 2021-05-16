24103074aa4c42b9aed9614f5e4d4d1e6e741b8b
anchita93gupta <anchita93gupta@gmail.com>
2021-05-16 13:20:28 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,48 @@
+var fixedRect, movingRect;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  
+  gameObject1 = createSprite(100, 100, 50, 50);
+  gameObject1.shapeColor = "green";
+  gameObject2 = createSprite(200, 100, 50, 50);
+  gameObject2.shapeColor = "green";
+  gameObject3 = createSprite(300, 100, 50, 50);
+  gameObject3.shapeColor = "green";
+  gameObject4 = createSprite(400, 100, 50, 50);
+  gameObject4.shapeColor = "green";
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  isTouching(movingRect,gameObject3)
+    
+  
   drawSprites();
+}
+function isTouching(obj1,obj2)
+{
+  if (obj1.x - obj2.x < obj2.width/2 + obj1.width/2
+    && obj2.x - obj1.x < obj2.width/2 + obj1.width/2
+    && obj1.y - obj2.y < obj2.height/2 + obj1.height/2
+    && obj2.y - obj1.y < obj2.height/2 + obj1.height/2) {
+
+      obj1.shapeColor = "red";
+      obj2.shapeColor = "red";
+ 
+}
+else {
+  obj1.shapeColor = "green";
+  obj2.shapeColor = "green";
+}
 }
